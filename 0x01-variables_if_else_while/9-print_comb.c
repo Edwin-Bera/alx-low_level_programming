16fd51eb9b3547b82583af8fbd197a4f1a732bf4
Edwin-Bera <blacksenyin@gmail.com>
2023-02-19 23:29:33 +0000

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if ( n <9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
