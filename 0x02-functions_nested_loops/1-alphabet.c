fb2165e605a10fb105e8bcc3a71ead9516486bb6
Edwin-Bera <blacksenyin@gmail.com>
2023-02-21 23:35:26 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	    print_alphabet();
+	        return (0);
+}
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	for (; letter <= 'z; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
