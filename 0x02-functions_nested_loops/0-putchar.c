4a359aca057cb86a323c9502717acffa64a3fa3d
Edwin-Bera <blacksenyin@gmail.com>
2023-02-21 23:13:04 +0000

    put~

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,9 @@ int main(void)
 	char put[] = "_putchar";
 	int index;
 
-	for(index = 0; put[index]; index++)
-	       _putchar(put[index]);
-	_putchar('\n');	
+	for (index = 0; put[index]; index++)
+		_putchar(put[index]);
+	_putchar('\n');
 
 	return (0);
 }
