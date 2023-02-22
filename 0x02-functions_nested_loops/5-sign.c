dd3768087d9591d8f7dee3f83d655361f2f8fb28
Edwin-Bera <blacksenyin@gmail.com>
2023-02-22 01:44:01 +0000

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign - prints sign of a number
+ * @n: the number to be evaluated
+ * Return: 1 if greater than 0, else returns 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
