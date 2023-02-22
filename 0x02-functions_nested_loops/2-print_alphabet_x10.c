d93b799ab453168375a3f53b2ba135759fe60490
Edwin-Bera <blacksenyin@gmail.com>
2023-02-22 01:00:59 +0000

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,10 @@ void print_alphabet_x10(void)
 	int n = 0;
 	char letter;
 
-	for (; n <= 10; n++)
+	for (; n < 10; n++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			 _putchar(letter);
 		_putchar('\n');
 	}
-
 }
