135302ecdd5f269345e4fe821bd62a5256e16a32
Edwin-Bera <blacksenyin@gmail.com>
2023-02-24 22:37:34 +0000

    numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prnts the numbers 0 - 14 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int outter;
+	int inner;
+
+	for (outter = 0; outter < 10; outter++)
+	{
+		for (inner = 0; inner < 14; innner++)
+		{
+			if (inner > 9)
+				_putchar('1');
+			_putchar(inner % 10);
+		}
+		_putchar('\n');
+	}
+}
