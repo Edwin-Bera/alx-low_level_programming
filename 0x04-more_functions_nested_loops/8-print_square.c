3bbe854d57bf9ce94f972883ad6c90bfd04f63c7
Edwin-Bera <blacksenyin@gmail.com>
2023-02-24 23:40:53 +0000

    the squares are winning

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - draws a square of the specified size
+ * @size: the length and width of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int index;
+	int inner;
+
+	for (index = 0; index < size; index++)
+	{
+		for (inner = 0; inner < size; inner++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
