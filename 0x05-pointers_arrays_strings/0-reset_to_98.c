2a0ebf237270d5bc69899c3538da4cb6218e5fa4
Edwin-Bera <blacksenyin@gmail.com>
2023-02-27 22:34:15 +0000

    0,1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - assigns the 98 to an int variable
+ * @n: the variable
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int -  swaps the values of two integers.
+ * @a: the fist int
+ * @b: the second int
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+
+	int *b = *a;
+	int *a = temp;
+}
