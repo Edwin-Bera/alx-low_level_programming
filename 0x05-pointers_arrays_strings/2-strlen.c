d511a86a139f50ea4890fb53b9071768732c7e10
Edwin-Bera <blacksenyin@gmail.com>
2023-02-28 22:59:34 +0000

    str.length()

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: points to the beginning of the string
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (s[counter] != '\0')
+		counter++;
+
+	return (counter);
+}
