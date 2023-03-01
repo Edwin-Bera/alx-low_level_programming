c57dc7032f28e6416f378944718e03b9cf6f1e88
Edwin-Bera <blacksenyin@gmail.com>
2023-03-01 00:42:27 +0000

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: the string
+ */
+void puts_half(char *str)
+{
+	int index = 0;
+	int half;
+
+	while (str[index] != '\0')
+		index++;
+	index -= 1;
+
+	if (index % 2)
+		half = (index - 1) / 2;
+	else
+		half = index / 2;
+
+	for (; half >= index; half++)
+		_putchar(str[half]);
+	-putchar('\n');
+}
