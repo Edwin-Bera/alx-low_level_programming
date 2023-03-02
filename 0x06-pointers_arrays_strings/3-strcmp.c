5f3f24cedc46fafa46d1e151feafad97c21e39a6
Edwin-Bera <blacksenyin@gmail.com>
2023-03-02 22:46:29 +0000

    cmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int index;
 
-	for (index = 0; s1[index] != '\0' && s2[index] != '\0' ; index++)
+	for (index = 0; s1[index] != '\0' || s2[index] != '\0' ; index++)
 	{
 		if (s1[index] > s2[index])
 			return (15);
