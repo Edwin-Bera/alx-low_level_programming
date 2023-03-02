5af76a79305765d77b37194807dbc7327e2719b2
Edwin-Bera <blacksenyin@gmail.com>
2023-03-02 23:27:36 +0000

    upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,6 +10,6 @@ char *string_toupper(char *c)
 	int index = 0;
 	for (; c[index] != '\0'; index++)
 		if (c[index] >= 'a' && c[index] <= 'z')
-			c[index] = 32;
+			c[index] -= 32;
 	return (c);
 }
