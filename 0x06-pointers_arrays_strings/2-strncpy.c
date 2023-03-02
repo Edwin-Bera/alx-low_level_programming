93a554d39d4f24fe5e912fad9d2a8bda4aaba83b
Edwin-Bera <blacksenyin@gmail.com>
2023-03-02 20:53:41 +0000

    strcpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - appends the string src to the begining of dest
+ * @dest: destinaton
+ * @src: source
+ * @n: number of characters o be copied
+ * Return: the full string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int counter = 0;
+
+	for (; src[counter] != '\0' && counter <= n - 1; counter++)
+	{
+		dest[counter] = src[counter];
+		if (src[counter] == '\n')
+		{
+			dest[counter + 1] = '\0';
+			break;
+		}
+	}
+
+	return (dest);
+}
