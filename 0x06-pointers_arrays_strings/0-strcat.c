206d00dc60dfafe8926b739657b81dde8f09465d
Edwin-Bera <blacksenyin@gmail.com>
2023-03-02 16:14:26 +0000

    string merge

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - appends the string src to dest
+ * @dest: destinaton
+ * @src: source
+ * Return: the full string
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest += src;
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+#what each program does
