cbb5db6b91e6ebfe08a76b07358fdaea5417ab9b
Edwin-Bera <blacksenyin@gmail.com>
2023-03-02 19:48:24 +0000

    betty fix 1,2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,12 @@ char *_strcat(char *dest, char *src)
 {
 	int counter = 0;
 	int i = 0;
-	
+
 	while (dest[counter] != '\0')
 		counter++;
 
-	for (i =0; src[i] != '\0'; i++)
+	for (; src[i] != '\0'; i++)
 		dest[counter + i] = src[i];
-	
+
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,12 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int counter = 0;
 	int i = 0;
-	
+
 	while (dest[counter] != '\0')
 		counter++;
 
 	for (; src[i] != '\0' && i <= n; i++)
 		dest[counter + i] = src[i];
-	
+
 	return (dest);
 }
