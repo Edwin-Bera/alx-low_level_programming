d304e5f462ffc682a3c8050a253214fbb92daf28
Edwin-Bera <blacksenyin@gmail.com>
2023-03-02 22:44:04 +0000

    cmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * returns an int greater than 0 if s1 > s2
+ * returns an int less than 0 if s1 < s2
+ * returns an int equal to 0 if s1 == s2
+ *
+ * @s1: first strung
+ * @s2: second string
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int index;
+
+	for (index = 0; s1[index] != '\0' && s2[index] != '\0' ; index++)
+	{
+		if (s1[index] > s2[index])
+			return (15);
+		else if (s1[index] < s2[index])
+			return (-15);
+	}
+	return (0);
+}
