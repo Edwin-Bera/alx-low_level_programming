fd46d82212bda82cddd7aed48806f771d9344c91
Edwin-Bera <blacksenyin@gmail.com>
2023-03-06 16:07:07 +0000

    first occurrence

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occurrence in the string s
+ * of any of the bytes in the string accept
+ * @s: the string
+ * @accept: the prefix substring
+ * Return: a pointer to the first match
+ */
+unsigned int _strpbrk(char *s, char *accept)
+{
+	int inner, index = 0;
+
+	for (; s[index] != ' ' && s[index]; index++)
+		for (inner = 0; accept[inner] != '\0'; inner++)
+			if (s[index] == accept[inner])
+				return (&s[index])
+	return ('\0');
+}
