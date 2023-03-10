d625ca708524cc230682a994c2129b8167e0ec2d
Edwin-Bera <blacksenyin@gmail.com>
2023-03-10 01:25:07 +0000

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * get_length - returns the length of a string
+ * @string: the string
+ * @size: index to transverse the string
+ * Return: length of string
+ */
+int get_length(char *string, int size)
+{
+	if (!(string[size]))
+		return (0);
+	else
+		return (1 + get_lenght(string, size + 1));
+}
+/**
+ * check_palindrome - checks whether a word is a palindrome or not
+ * @string: string containing the word
+ * @size: size of the string
+ * Return: 1 if the string is a palindrome, else 0
+ */
+int check_palindrome(char *string, int size)
+{
+	if (size - 1 == 1)
+		return (1);
+	else if (*string == string[size - 1])
+		return (check_palindrome(string + 1, size - 1));
+	else
+		return (0);
+}
+/**
+ * is_palindrome -  returns 1 if a string is a palindrome and 0 if not.
+ * @s: the string
+ * Return: 1 if palindrome, else 0
+ */
+
+int is_palindrome(char *s)
+{
+	int length = 0;
+
+	length = get_length(s, length);
+	if (length <= 1)
+		return (1);
+	else
+		return (check_palindrome(s, length));
+}
+
