fa5700050272f5d2fd50e5f9109ea7dbd078b2b2
Edwin-Bera <blacksenyin@gmail.com>
2023-03-15 22:53:36 +0000

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,22 +14,27 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
-	while (s1[size1])
-		size1++;
+	if (s1 != NULL)
+		while (s1[size1])
+			size1++;
 
-	while (s2[size2])
-		size2++;
+	if (s2 != NULL)
+		while (s2[size2])
+			size2++;
+	size2++;
 
-	ptr = malloc(size1 + size2 + 1);
+	ptr = malloc(size1 + size2);
 	if (ptr == NULL)
 		return (NULL);
 
 	for (; i < size1; i++)
 		ptr[i] = s1[i];
 
-	for (i = 0; i < size2; i++)
-		ptr[size1 + i] = s2[i];
-	ptr[size1 + i] = '\0';
+	if (size2 == 1)
+		ptr[size1] = '\0';
+	else
+		for (i = 0; i < size2; i++)
+			ptr[size1 + i] = s2[i];
 
 	return (ptr);
 }
