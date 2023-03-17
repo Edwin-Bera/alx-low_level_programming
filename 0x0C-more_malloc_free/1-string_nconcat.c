0fe855d756f902ccbfc199b5f2c360079185a38d
Edwin-Bera <blacksenyin@gmail.com>
2023-03-17 00:40:52 +0000

    done?

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (num2 + num1 == 0)
 		return ('\0');
-	if (n < num2)
+	if (n < num2 - 1)
 		num2 = n;
 
 	str = malloc(num1 + num2);
