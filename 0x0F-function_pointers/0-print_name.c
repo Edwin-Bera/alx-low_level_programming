e93f48e60e18fcdcf23e88368870fb18b53384e3
Edwin-Bera <blacksenyin@gmail.com>
2023-03-22 19:42:44 +0000

    null handling added

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - prints a name defined by the string passed to it
  * @name: the sstring in question
@@ -6,5 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL)
+		f(name);
 }
