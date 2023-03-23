7cfa0d5abaaf1c6f1d075f1382bc82f2037274be
Edwin-Bera <blacksenyin@gmail.com>
2023-03-23 20:30:09 +0000

    ADD 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (i + 1 != n && separator != NULL)
 				printf("%s", separator);
 		}
-		printf("\n");
 		va_end(ptr);
 	}
+	printf("\n");
 }
