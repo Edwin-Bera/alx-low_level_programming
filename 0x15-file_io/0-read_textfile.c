f771386c5c6d47fcd1e620d65608790abec3d0bb
Edwin-Bera <blacksenyin@gmail.com>
2023-04-10 22:52:04 +0000

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,15 +7,15 @@
  * read_textfile - prints out contents of a text file
  * @filename: name of file
  * @letters: number of bytees to be printed
- * Return: 0 if it fails, else ereturns number of bytes printed
+ * -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-read_textfile.c -o return: 0 if it fails, else ereturns number of bytes printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int readFile = open(filename, O_RDONLY);
-	char text[letters];
+	char *text = malloc(letters);
 	ssize_t num;
 
-	if (readFile < 0)
+	if (readFile < 0 || text == NULL)
 		return (0);
 
 	num = read(readFile, text, letters);
@@ -26,7 +26,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	close(readFile);
 
-	if (write(2, &text, letters) < letters)
+	num = write(2, text, letters);
+	free(text);
+	if ((long unsigned int)num < letters)
 		return (0);
 
 	return (letters);
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,9 @@
 int create_file(const char *filename, char *text_content)
 {
 	int err;
-	int file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	int file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
 
-	if (file == -1)
+	if (file == -1 || text_content == NULL)
 		return (-1);
 
 	err = write(file, text_content, strlen(text_content));
