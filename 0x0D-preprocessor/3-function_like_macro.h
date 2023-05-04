2bef1af627f65f3b34e439d1c8d22068769bcd22
Edwin-Bera <blacksenyin@gmail.com>
2023-05-04 12:16:06 +0000

    purge repository from recursion going forward

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,4 +0,0 @@
-#ifndef ABSOLUTE_H
-#define ABSOLUTE_H
-#define ABS(x) (((x) < (0)) ? -(x) : (x))
-#endif
--- 0x10-variadic_functions/main.c
+++
@@ -1,7 +0,0 @@
-#include "variadic_functions.h"
-#include <stddef.h>
-int main(void)
-{
-	    print_strings(", ", 2, NULL, "Django");
-	        return (0);
-}
