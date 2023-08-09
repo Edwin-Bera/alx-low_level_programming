1bcf1cc7a9c461d9adc360c9420623d383087dae
Edwin-Bera <blacksenyin@gmail.com>
2023-08-09 19:51:28 +0000

    ADD argc_argv project

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  adds positive integers
+ * @argc: number of arguments
+ * @argv: arrary containing arguments
+ * Return: 1 if an invalid character is detected, else return 0
+ */
+int main(int argc, char **argv)
+{
+	int num = 0;
+	int i = 1;
+	int inner;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (; i < argc; i++)
+	{
+		for (inner = 0; argv[i][inner]; inner++)
+			if (!(argv[i][inner] >= '0' && argv[i][inner] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		num += atoi(argv[i]);
+	}
+	printf("%d\n", num);
+	return (0);
+}
