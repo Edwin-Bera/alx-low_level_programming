211619ffb66ba288af3db9ed4e9f3998e35cb3c6
Edwin-Bera <blacksenyin@gmail.com>
2023-08-10 19:22:49 +0000

    ADD more_malloc project

---
+++ 0x0B-malloc_free/0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _memset - fills the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ * @s: char array (input)
+ * @b: the fill in character
+ * @n: bytes of memory to be filled in
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: pointer to the allocated memory.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (size < 1 || nmemb < 1)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
