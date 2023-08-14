c0f3bd1938fb9399e1e96fec57d2a5d2ae72ee5d
Edwin-Bera <blacksenyin@gmail.com>
2023-08-14 21:48:55 +0000

    ADD structures questions 1-3

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints out the value of vaariable of type dog
+ * @d - a variable of type dog
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: ");
+		if (d->name == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		printf("Owner: ");
+		if (d->owner == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", d->owner);
+	}
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - holds information pertaining to a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
